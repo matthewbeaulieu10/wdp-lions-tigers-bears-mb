c393ef347d78f25a4dd3874a021dc1560a6d39b3
matthew <matthewbeaulieu10@gmail.com>
2022-09-29 18:47:15 -0500

    finished tutorial, got random dog pictures to show from a different api. needed to push more often.

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
-async function start() {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json()
-    createBreedList(data.message)
+let timer
+let deleteFirstPhotoDelay
+
+// async function start() {
+    
+//         const response = await fetch("https://random.dog/woof.json")
+//         const data = await response.json()
+//         console.log(data)
+    
+    
+// }
+
+//start()
+
+function getAnimal() {
+    animal = document.getElementById("list").value
+    showAnimal(animal)
 }
 
-start()
-
-function createBreedList(breedList) {
-    document.getElementById("breed").innerHTML = 
-    `
-        <select onchange="loadByBreed(this.value)">
-            <option>Choose a dog breed</option>
-            ${Object.keys(breedList).map(function (breed) {
-                return `<option>${breed}</option>`
-            }).join('')}
-        </select>
-    `
+async function showAnimal(animal) { 
+    if (animal == "d") {
+        const response = await fetch("https://random.dog/woof.json")
+        const data = await response.json()
+        console.log(data.url)
+        try {
+            document.getElementById("slideshow").innerHTML = `
+            <div class="slide" style="background-image:url('${data.url}')"></div>
+            `
+        } catch {
+            console.log("uh oh")
+        }
+    }
 }
 
-async function loadByBreed(breed) {
+async function loadByAnimal() {
     if (breed != "Choose a dog breed") {
         const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json()
-        console.log(data.message)
-        alert("this is working rn")
+        createSlideshow(data.message)
     }
 }
+
+// function createSlideshow(images) {
+//     let now = 0
+//     clearInterval(timer)
+//     clearTimeout(deleteFirstPhotoDelay)
+//     document.getElementById("slideshow").innerHTML = `
+//     <div class="slide" style="background-image: url('${images[0]}')"></div>
+//     <div class="slide" style="background-image: url('${images[1]}')"></div>
+//     `
+//     now += 2 
+//     timer = setInterval(nextSlide, 3000)
+    
+//     function nextSlide() {
+//         document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[now]}')"></div>`)
+//         deleteFirstPhotoDelay = setTimeout(removeSLide(), 1000)
+//         if (now + 1 >= images.length) {
+//             now = 0
+//         } else {
+//             now++
+//         }
+    
+//     }
+
+//     function removeSlide() {
+//         document.querySelector(".slide").remove()
+//     }
+
+    
+// }
+
