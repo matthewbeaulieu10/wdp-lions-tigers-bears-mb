818350503a3bd7ff610d40005a4c20bb168b7818
matthew <matthewbeaulieu10@gmail.com>
2022-09-29 22:04:18 -0500

    added working API functionality to dog, cat, and fox options. couldn't get duck to work

--- index.js
+++ index.js
@@ -1,71 +1,40 @@
-let timer
-let deleteFirstPhotoDelay
-
-// async function start() {
-    
-//         const response = await fetch("https://random.dog/woof.json")
-//         const data = await response.json()
-//         console.log(data)
-    
-    
-// }
-
-//start()
-
+//retrieves the value from the drop down menu and passes it to show animal
 function getAnimal() {
     animal = document.getElementById("list").value
     showAnimal(animal)
 }
 
+//determines what kinda animal the user wants
+//accesses the data from that API
+//creates a slide element inside the slideshow div using the data
 async function showAnimal(animal) { 
     if (animal == "d") {
+        
         const response = await fetch("https://random.dog/woof.json")
         const data = await response.json()
-        console.log(data.url)
-        try {
-            document.getElementById("slideshow").innerHTML = `
-            <div class="slide" style="background-image:url('${data.url}')"></div>
-            `
-        } catch {
-            console.log("uh oh")
-        }
+        
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image:url('${data.url}')"></div>
+        `     
     }
-}
 
-async function loadByAnimal() {
-    if (breed != "Choose a dog breed") {
-        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    else if (animal == "c"){
+        const response = await fetch("https://aws.random.cat/meow")
         const data = await response.json()
-        createSlideshow(data.message)
+        
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image:url('${data.file}')"></div>
+        `     
+    }
+
+    else if (animal == "f"){
+        const response = await fetch("https://randomfox.ca/floof/")
+        const data = await response.json()
+
+    
+        
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image:url('${data.image}')"></div>
+        `     
     }
 }
-
-// function createSlideshow(images) {
-//     let now = 0
-//     clearInterval(timer)
-//     clearTimeout(deleteFirstPhotoDelay)
-//     document.getElementById("slideshow").innerHTML = `
-//     <div class="slide" style="background-image: url('${images[0]}')"></div>
-//     <div class="slide" style="background-image: url('${images[1]}')"></div>
-//     `
-//     now += 2 
-//     timer = setInterval(nextSlide, 3000)
-    
-//     function nextSlide() {
-//         document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[now]}')"></div>`)
-//         deleteFirstPhotoDelay = setTimeout(removeSLide(), 1000)
-//         if (now + 1 >= images.length) {
-//             now = 0
-//         } else {
-//             now++
-//         }
-    
-//     }
-
-//     function removeSlide() {
-//         document.querySelector(".slide").remove()
-//     }
-
-    
-// }
-
